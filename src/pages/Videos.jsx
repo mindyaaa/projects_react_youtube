433bbf53afd3110586c18a63d0f0b8dd6addc0aa
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2023-02-12 19:55:04 +0900

    video 데이터 정보

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
 export default function Videos() {
     let {keyword} = useParams();
+    const [videoData, setVideoData] = useState([]);
+
+    useEffect(() => {
+        fetch('./videos/popular.json')
+        .then((res)=> res.json())
+        .then((data) => {
+            setVideoData(data.items);
+            // console.log(videoData.map((i) => i.snippet.title))
+            // console.log(data.items[0]);
+            // console.log(data.items[0].snippet.title)
+        });
+    },[])
 
     return (
         <div>
-            VIDEOS : {keyword ? `${keyword}` : '🔥'}
+            <h3>VIDEOS : {keyword ? `${keyword}` : '🔥'}</h3>
+            <ul>
+                {videoData.map((i) => 
+                    <li key={i.id}>{i.snippet.title}</li>
+                )}
+            </ul>
         </div>
     );
 }

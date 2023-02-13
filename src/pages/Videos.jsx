ee1fa79405cdb7221f3a821e7f034e3dd6b919f0
mindyaaa <107449479+mindyaaa@users.noreply.github.com>
2023-02-13 14:06:48 +0900

    react query 적용

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,29 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import VideoCard from '../components/VideoCard';
 
 export default function Videos() {
-    let {keyword} = useParams();
-    const [videoData, setVideoData] = useState([]);
+    let { keyword } = useParams();
+    const { isLoading, error, data:videos } = useQuery(
+        ['videos', keyword], async () => {
+            return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
+            .then((res) => res.json())
+            .then((data) => data.items)});
 
-    useEffect(() => {
-        fetch('./videos/popular.json')
-        .then((res)=> res.json())
-        .then((data) => {
-            setVideoData(data.items);
-            // console.log(videoData.map((i) => i.snippet.title))
-            // console.log(data.items[0]);
-            // console.log(data.items[0].snippet.title)
-        });
-    },[])
+
+    // const [videoData, setVideoData] = useState([]);
+
+    // useEffect(() => {
+    //     fetch('./videos/popular.json')
+    //     .then((res)=> res.json())
+    //     .then((data) => {
+    //         setVideoData(data.items);
+    //         // console.log(videoData.map((i) => i.snippet.title))
+    //         // console.log(data.items[0]);
+    //         // console.log(data.items[0].snippet.title)
+    //     });
+    // },[])
 
     return (
         <div>
             <h3>VIDEOS : {keyword ? `${keyword}` : '🔥'}</h3>
+            {isLoading && <p>Loading...</p>}
+            {error && <p>Something went WRONG</p>}
+            {videos && 
+            (
             <ul>
-                {videoData.map((i) => 
-                    <li key={i.id}>{i.snippet.title}</li>
+                {videos.map((video) => 
+                    <VideoCard key={video.id} video={video}></VideoCard>
                 )}
             </ul>
+            )          
+            }
+            
         </div>
     );
 }
